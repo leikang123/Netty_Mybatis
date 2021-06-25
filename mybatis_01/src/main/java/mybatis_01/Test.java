c96b4dd73fb8534eef5c33cad639f16bb1c85004
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-06-25 13:37:51 +0800

    Update Test.java

--- mybatis_01/src/main/java/mybatis_01/Test.java
+++ mybatis_01/src/main/java/mybatis_01/Test.java
@@ -1,10 +1,12 @@
 package mybatis_01;
 
+// 测试类
 public class Test {
 	public static void main(String[] args) {
 		
-		
+		// 创建对象并获取对象
 		Test studentDAO = new MyInvocationHandler().getInstance(Test.class);
+		// 获取对象的ID
 		Student stu = studentDAO.getById(1);
 	}
 
